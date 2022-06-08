d130eab4bca2c27e1dbde09a809682a92185fbf7
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-06-09 00:20:29 +0200

    WIP: Create html

---
+++ script.js
@@ -0,0 +1,8 @@
+const guessContainer = document.getElementById('current-guess');
+const tableOfGuesses = document.getElementById('table-of-guesses');
+const guessNumberContainer = document.getElementById(
+  'table-of-guesses-guess-number'
+);
+const guessGuessContainer = document.getElementById(
+  'table-of-guesses-guess-guess'
+);
